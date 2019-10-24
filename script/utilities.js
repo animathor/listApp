e3a96ee5a070574ad945728d7e365ec0636c7bf9
animath <alan1516251hotmail.com>
2019-10-24 14:36:28 +0800

    Pull the commom use function out and put them together

---
+++ script/utilities.js
@@ -0,0 +1,61 @@
+function find_ancestor_class(element, classname){
+		let targetEle = element;
+			do{
+					targetEle = targetEle.parentNode;
+			}while(!targetEle.className.match(classname));
+		return targetEle;
+}
+
+function find_ancestor_tag(element, tagname){
+		let targetEle = element;
+			do{
+					targetEle = targetEle.parentNode;
+					var checkTagName = targetEle.tagName.toLowerCase();
+			}while( checkTagName != tagname && checkTagName != 'body');
+		return targetEle;
+}
+
+function find_1stDescendant_with(element, tagname, classname){
+	let tagElements = element.getElementsByTagName(tagname);
+	let i;
+	for(i=0;i<tagElements.length;i++){
+		if(tagElements[i].className.match(classname))
+			break;
+	}
+	if(i<tagElements.length){
+		return tagElements[i];
+	}else{	
+		return null;
+	}
+}
+
+function getTriggerEle(e){
+	if(!e){
+			e = window.event;
+		}
+	element = e.target || e.srcElement;
+	return	element;
+}
+
+function preventLinkAction(e){
+		if(e.preventDefault){
+			e.preventDefault();
+		}else{
+			e.returnValue = false;
+		}
+}
+
+function stopBubbling(e){
+		//stop bubbling
+		if(e.stopPropogation){
+			e.stopPropogation();
+		}else{
+			e.cancelBubble = true;
+		}
+}
+// modify the code in MDN document about 'encodeURIComponent'
+function encodeForPost(str) {
+  return encodeURIComponent(str).replace('%20','+').replace(/[!'()*]/g, function(c) {
+    return '%' + c.charCodeAt(0).toString(16);
+  });
+}
