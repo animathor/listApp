2c6e00d081827e54185c09176779076686c40977
animath <alan1516251hotmail.com>
2019-10-27 00:15:14 +0800

    Combine the hide event script in to control.js and individual *_conttol.js

--- script/hide.js
+++
@@ -1,9 +0,0 @@
-function toggleHide(e){
-		let className = e.className;
-		if(className.match('hide')){
-			e.className = className.replace(' hide', '');
-		}else{
-			e.className += ' hide';
-			console.log(e.className);
-		}
-}
